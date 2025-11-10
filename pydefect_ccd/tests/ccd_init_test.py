09fce9c0b4b17f56017a8dc50e293a8a44dda4db
yuuukuma <yuuukuma@gmail.com>
2025-11-11 05:07:38 +0900

    modify test

--- pydefect_ccd/tests/ccd_init_test.py
+++ pydefect_ccd/tests/ccd_init_test.py
@@ -11,21 +11,17 @@ def test_json_roundtrip(ccd_init, tmpdir):
     assert_json_roundtrip(ccd_init, tmpdir)
 
 
-def test_minimum_point_info_degeneracy_by_symm_reduction(relaxed_point):
-    assert relaxed_point.degeneracy_by_symmetry_reduction == 2
-
-
-def test_dephon_init_dQ(ccd_init):
+def test_init_dQ(ccd_init):
     expected = sqrt((0.1*10)**2*6 * Element.H.atomic_mass)
     assert ccd_init.dQ == pytest.approx(expected)
 
 
-def test_dephon_init_min_point_info_from_charge(ccd_init):
+def test_relaxed_point_from_charge(ccd_init):
     actual = ccd_init.relaxed_point_from_charge(charge=1)
     assert actual == ccd_init.relaxed_points[1]
 
 
-def test_dephon_init_volume(ccd_init):
+def test_volume(ccd_init):
     assert ccd_init.volume == 1000.0
 
 
